a27c2a27883fe10c902df66a0cebf9dd4d9e5b8c
BuildTools <unconfigured@null.spigotmc.org>
2016-12-20 20:49:07 +0900

    釣りプラグイン

--- TuripPlugin.java
+++ TuripPlugin.java
@@ -13,8 +13,8 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TuripPlugin extends JavaPlugin implements Listener {
-	int point=0;
-
+	int point1=0;
+	int point2=0;
 	@Override
 	public void onEnable() {
 	this.getServer().getPluginManager().registerEvents(this, this);}
@@ -28,25 +28,29 @@ public class TuripPlugin extends JavaPlugin implements Listener {
 
 
 		if (player.isOp() && event.getAction() == Action.RIGHT_CLICK_BLOCK &&
-				item.getType() == Material.STICK && point == 0) {
-			point=1;
-			List<String> blockpoint = new ArrayList<String>();
-			blockpoint.add(String.valueOf(x));
-			blockpoint.add(String.valueOf(y));
-
-
-
-
-
-
-
+				item.getType() == Material.STICK && point1 == 0) {
+			point1=1;
+			List<String> blockpoint1 = new ArrayList<String>();
+			blockpoint1.add(String.valueOf(x));
+			blockpoint1.add(String.valueOf(y));
+			player.sendMessage(""+blockpoint1+"にX座標を"+x+"Y座標を"+y+"と記録しました");
+			point1=0;
+		}
+		if (player.isOp() && event.getAction() == Action.LEFT_CLICK_BLOCK &&
+				item.getType() == Material.STICK && point2 == 0) {
+			point2=1;
+			List<String> blockpoint2 = new ArrayList<String>();
+			blockpoint2.add(String.valueOf(x));
+			blockpoint2.add(String.valueOf(y));
+			player.sendMessage(""+blockpoint2+"にX座標を"+x+"Y座標を"+y+"と記録しました");
+			point2=0;
 		}
 
 	}
 
 
-
-
-
-
 }
+
+
+
+
